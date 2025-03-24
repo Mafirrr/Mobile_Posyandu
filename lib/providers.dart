21d90079ec4f29dcc96a83dd4a355e84f13d8835
newbieflank <raninovia188@gmail.com>
2025-03-25 05:55:31 +0700

    delete file

--- lib/providers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:posyandu_mob/core/viewmodel/login_viewmodel.dart';
-import 'package:provider/provider.dart';
-import 'package:provider/single_child_widget.dart';
-// import 'core/viewmodels/dashboard_viewmodel.dart';
-
-List<SingleChildWidget> providers = [
-  ChangeNotifierProvider(create: (context) => LoginViewModel()),
-  // ChangeNotifierProvider(create: (context) => DashboardViewModel()),
-];
