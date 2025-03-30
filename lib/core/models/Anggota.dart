ed540388d582856757fcfdebb35b379a2635aa25
Mafirrr <firaaurelsf@gmail.com>
2025-03-30 13:45:42 +0700

    tes

---
+++ lib/core/models/Anggota.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:json_annotation/json_annotation.dart';
+
+part 'Anggota.g.dart';
+
+@JsonSerializable()
+class Anggota {
+  int id;
+  String nik;
+  String nama;
+  String tanggal_lahir;
+  String tempat_lahir;
+  String pekerjaan;
+  String alamat;
+  String no_telepon;
+  String golongan_darah;
+  Anggota({
+    required this.id,
+    required this.nik,
+    required this.nama,
+    required this.tanggal_lahir,
+    required this.tempat_lahir,
+    required this.pekerjaan,
+    required this.alamat,
+    required this.no_telepon,
+    required this.golongan_darah,
+  });
+
+  factory Anggota.fromJson(Map<String, dynamic> json) =>
+      _$AnggotaFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AnggotaToJson(this);
+}
