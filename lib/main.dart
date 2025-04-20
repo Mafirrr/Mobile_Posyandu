a8b76d29d20acde4a3f65e2b01052a7c28c95da0
newbieflank <raninovia188@gmail.com>
2025-04-20 11:14:06 +0700

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
-import 'dart:convert';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:posyandu_mob/core/services/auth_service.dart';
 import 'package:posyandu_mob/core/viewmodel/auth_viewmodel.dart';
 import 'package:posyandu_mob/core/viewmodel/profile_viewmodel.dart';
-import 'package:posyandu_mob/screens/login/login_screen.dart';
-import 'package:posyandu_mob/screens/navigation/navAnggota_screen.dart';
+import 'package:posyandu_mob/screens/SplashScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,48 +25,14 @@ void main() async {
   );
 }
 
-Future<Map<String, dynamic>?> checkLoginStatus() async {
-  return await _getUser();
-}
-
-Future<dynamic> _getUser() async {
-  final prefs = await SharedPreferences.getInstance();
-  final String? userData = prefs.getString(AuthService.userKey);
-
-  if (userData != null) {
-    final Map<String, dynamic> userMap = jsonDecode(userData);
-    return userMap;
-  }
-  return null;
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FutureBuilder<Map<String, dynamic>?>(
-        future: checkLoginStatus(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          } else if (snapshot.hasData && snapshot.data != null) {
-            final userMap = snapshot.data!;
-
-            if (userMap['role'] == "anggota") {
-              return const NavAnggotaScreen();
-            } else {
-              return const LoginScreen();
-            }
-          } else {
-            return const LoginScreen();
-          }
-        },
-      ),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:posyandu_mob/core/services/auth_service.dart';
+import 'package:posyandu_mob/screens/login/login_screen.dart';
+import 'package:posyandu_mob/screens/navigation/navAnggota_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController bgController;
+  late Animation<double> bgAnimation;
+
+  late AnimationController logoController;
+  late Animation<double> logoOpacity;
+  late Animation<double> logoScale;
+  late Animation<Offset> logoOffset;
+
+  late AnimationController textController;
+  late Animation<double> textOpacity;
+
+  late AnimationController fadeOutController;
+  late Animation<double> fadeOutAnimation;
+
+  bool showSplash = true;
+  bool? isLoggedIn; // Status login user
+
+  // Durasi animasi
+  final backgroundDuration = const Duration(milliseconds: 1000);
+  final delayAfterBackground = const Duration(milliseconds: 200);
+  final logoDuration = const Duration(milliseconds: 800);
+  final textDuration = const Duration(milliseconds: 800);
+  final delayAfterText = const Duration(milliseconds: 800);
+  final fadeOutDuration = const Duration(milliseconds: 400);
+
+  @override
+  void initState() {
+    super.initState();
+    checkLoginStatus();
+
+    bgController = AnimationController(
+      duration: backgroundDuration,
+      vsync: this,
+    );
+    bgAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: bgController, curve: Curves.easeOut),
+    );
+
+    logoController = AnimationController(
+      duration: logoDuration,
+      vsync: this,
+    );
+    logoOpacity = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: logoController, curve: Curves.easeIn),
+    );
+    logoScale = Tween<double>(begin: 0.6, end: 1.0).animate(
+      CurvedAnimation(parent: logoController, curve: Curves.easeOutBack),
+    );
+
+    textController = AnimationController(
+      duration: textDuration,
+      vsync: this,
+    );
+    textOpacity = Tween<double>(begin: 0, end: 1).animate(textController);
+
+    logoOffset = Tween<Offset>(
+      begin: const Offset(0, 0.1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: textController, curve: Curves.easeOut));
+
+    fadeOutController = AnimationController(
+      duration: fadeOutDuration,
+      vsync: this,
+    );
+    fadeOutAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(parent: fadeOutController, curve: Curves.easeOut),
+    );
+
+    runAnimation(); // Mulai animasi
+  }
+
+  Future<bool> checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? userData = prefs.getString(AuthService.userKey);
+
+    if (userData != null) {
+      return true;
+    }
+    return false;
+  }
+
+  // Fungsi untuk menjalankan animasi
+  Future<void> runAnimation() async {
+    await bgController.forward();
+    await Future.delayed(delayAfterBackground);
+    await logoController.forward();
+    await textController.forward();
+    await Future.delayed(delayAfterText);
+    await fadeOutController.forward();
+    setState(() {
+      showSplash = false;
+    });
+
+    if (isLoggedIn == true) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const NavAnggotaScreen()),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    bgController.dispose();
+    logoController.dispose();
+    textController.dispose();
+    fadeOutController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          if (isLoggedIn == true)
+            const NavAnggotaScreen()
+          else if (isLoggedIn == false)
+            const LoginScreen()
+          else
+            const SizedBox(),
+          if (showSplash)
+            FadeTransition(
+              opacity: fadeOutAnimation,
+              child: AnimatedBuilder(
+                animation: bgAnimation,
+                builder: (context, child) {
+                  return CustomPaint(
+                    painter: ExpandingCirclePainter(
+                        animationValue: bgAnimation.value),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SlideTransition(
+                            position: logoOffset,
+                            child: ScaleTransition(
+                              scale: logoScale,
+                              child: FadeTransition(
+                                opacity: logoOpacity,
+                                child: Image.asset(
+                                  'assets/images/logo_putih.png',
+                                  width: 120,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 0),
+                          FadeTransition(
+                            opacity: textOpacity,
+                            child: const Text(
+                              "Posyandu",
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class ExpandingCirclePainter extends CustomPainter {
+  final double animationValue;
+
+  ExpandingCirclePainter({required this.animationValue});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final radius = size.longestSide * animationValue;
+    final paint = Paint()..color = Colors.blue;
+    canvas.drawCircle(size.center(Offset.zero), radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant ExpandingCirclePainter oldDelegate) {
+    return oldDelegate.animationValue != animationValue;
+  }
+}
